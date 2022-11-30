f0557e9c6771f842754439d5aca3984a2370697e
Eli Richardson <elijahrich02@icloud.com>
2022-11-29 18:28:52 -0600

    gzip and beta 1

--- back/main.go
+++ back/main.go
@@ -6,11 +6,13 @@ import (
 	"github.com/eli-rich/gobin/api/db"
 	"github.com/eli-rich/gobin/api/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/nanmu42/gzip"
 )
 
 func main() {
 	db.Initalize()
 	r := gin.Default()
+	r.Use(gzip.DefaultHandler().Gin)
 	routes.LoadRoutes(r)
 	r.NoRoute(gin.WrapH(http.FileServer(http.Dir("./front/dist"))))
 	r.GET("/:slug", func(c *gin.Context) {
--- front/src/components/Editor/EditorWrapper.tsx
+++ front/src/components/Editor/EditorWrapper.tsx
@@ -1,22 +1,31 @@
 import { basicSetup } from 'codemirror';
 import { EditorView } from '@codemirror/view';
 import { gruvboxDark } from 'cm6-theme-gruvbox-dark';
-import { onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import EditorOptions from './EditorOptions';
+import Modal from '../Modal';
+import { A } from '@solidjs/router';
 
 export default function EditorWrapper() {
   let editorParent!: HTMLDivElement;
   let editor: EditorView;
 
+  const [showLink, setShowLink] = createSignal<boolean>(true);
+  const [link, setLink] = createSignal<string>('');
+
   const handleUpload = async () => {
-    const result = await fetch('http://localhost:3000/api/post', {
+    const result = await fetch('/api/post', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ content: editor.state.doc.toJSON().join('\n') }),
     });
+    const data = await result.json();
+    setLink(`/${data.message}`);
+    setShowLink(true);
   };
+
   onMount(() => {
     const theme = EditorView.baseTheme({
       '&': {
@@ -34,6 +43,20 @@ export default function EditorWrapper() {
   return (
     <>
       <div ref={editorParent} class='bg-[#282828] min-h-[66vh] h-fit mx-auto w-[80%]' />
+      <Modal
+        htmlName='link-modal'
+        show={showLink}
+        showHandler={setShowLink}
+        title='File saved successfully!'
+        centered={true}
+      >
+        <p class='text-xl'>
+          View your file:{' '}
+          <A href={link()} class='text-accent underline decoration-secondary'>
+            here
+          </A>
+        </p>
+      </Modal>
       <EditorOptions handleUpload={handleUpload} />
     </>
   );
