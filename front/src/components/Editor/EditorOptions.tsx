39437e9decbd63728df09a7c6ac1ca8b554ed132
Eli Richardson <elijahrich02@icloud.com>
2022-12-03 12:18:08 -0600

    update langs

--- front/src/components/Editor/EditorOptions.tsx
+++ front/src/components/Editor/EditorOptions.tsx
@@ -1,20 +1,26 @@
 interface EditorOptionsProps {
   handleUpload: () => Promise<void>;
+  updateLang: (e: Event) => void;
 }
 export default function EditorOptions(props: EditorOptionsProps) {
   return (
     <>
       <div class='flex gap-4'>
-        <select class='select select-secondary w-[10rem]'>
+        <select class='select select-secondary w-[10rem]' onInput={props.updateLang}>
           <option selected>Text</option>
-          <option disabled>Java</option>
-          <option disabled>Go</option>
-          <option disabled>C</option>
-          <option disabled>C#</option>
-          <option disabled>C++</option>
-          <option disabled>Rust</option>
-          <option disabled>JavaScript</option>
-          <option disabled>Python</option>
+          <option>Markdown</option>
+          <option>JavaScript</option>
+          <option>HTML</option>
+          <option>CSS</option>
+          <option>JSON</option>
+          <option>TypeScript</option>
+          <option>JSX</option>
+          <option>TSX</option>
+          <option>Python</option>
+          <option>PHP</option>
+          <option>Java</option>
+          <option>C++</option>
+          <option>Rust</option>
         </select>
         <button class='btn btn-secondary btn-outline w-[10rem]' onClick={props.handleUpload}>
           Save
--- front/src/components/Editor/EditorWrapper.tsx
+++ front/src/components/Editor/EditorWrapper.tsx
@@ -1,10 +1,27 @@
-import { basicSetup } from 'codemirror';
-import { EditorView } from '@codemirror/view';
-import { gruvboxDark } from 'cm6-theme-gruvbox-dark';
-import { createSignal, onMount } from 'solid-js';
+import { createEffect, createSignal } from 'solid-js';
 import EditorOptions from './EditorOptions';
 import Modal from '../Modal';
 import { A } from '@solidjs/router';
+
+import { basicSetup } from 'codemirror';
+import { EditorView } from '@codemirror/view';
+import { gruvboxDark } from 'cm6-theme-gruvbox-dark';
+import {
+  javascriptLanguage,
+  typescriptLanguage,
+  jsxLanguage,
+  tsxLanguage,
+} from '@codemirror/lang-javascript';
+import { pythonLanguage } from '@codemirror/lang-python';
+import { htmlLanguage } from '@codemirror/lang-html';
+import { cppLanguage } from '@codemirror/lang-cpp';
+import { LRLanguage } from '@codemirror/language';
+import { cssLanguage } from '@codemirror/lang-css';
+import { rustLanguage } from '@codemirror/lang-rust';
+import { jsonLanguage } from '@codemirror/lang-json';
+import { markdownLanguage } from '@codemirror/lang-markdown';
+import { javaLanguage } from '@codemirror/lang-java';
+import { phpLanguage } from '@codemirror/lang-php';
 
 export default function EditorWrapper() {
   let editorParent!: HTMLDivElement;
@@ -14,6 +31,9 @@ export default function EditorWrapper() {
   const [link, setLink] = createSignal<string>('');
   const [showErrModal, setShowErrModal] = createSignal<boolean>(false);
   const [err, setErr] = createSignal<string>('');
+
+  const [lang, setLang] = createSignal<string>('');
+
   let title!: HTMLInputElement;
   const handleUpload = async () => {
     let result: Response;
@@ -41,17 +61,83 @@ export default function EditorWrapper() {
     setShowLink(true);
   };
 
-  onMount(() => {
-    const theme = EditorView.baseTheme({
-      '&': {
-        width: '100%',
-        margin: '2rem auto 0 auto',
-      },
-    });
+  const theme = EditorView.baseTheme({
+    '&': {
+      width: '100%',
+      margin: '2rem auto 0 auto',
+    },
+  });
+
+  const updateLang = (e: Event) => {
+    setLang((e.target as HTMLSelectElement).value);
+  };
+  createEffect(() => {
+    if (!editor) {
+      editor = new EditorView({
+        doc: '',
+        parent: editorParent,
+        extensions: [basicSetup, theme, gruvboxDark],
+      });
+    }
+    const content = editor.state.doc.toJSON().join('\n');
+    let language: LRLanguage | undefined;
+    switch (lang()) {
+      case 'Markdown':
+        language = markdownLanguage as LRLanguage;
+        break;
+      case 'JavaScript':
+        language = javascriptLanguage;
+        break;
+      case 'Python':
+        language = pythonLanguage;
+        break;
+      case 'TypeScript':
+        language = typescriptLanguage;
+        break;
+      case 'JSX':
+        language = jsxLanguage;
+        break;
+      case 'TSX':
+        language = tsxLanguage;
+        break;
+      case 'HTML':
+        language = htmlLanguage;
+        break;
+      case 'CSS':
+        language = cssLanguage;
+        break;
+      case 'JSON':
+        language = jsonLanguage;
+        break;
+      case 'C++':
+        language = cppLanguage;
+        break;
+      case 'Rust':
+        language = rustLanguage;
+        break;
+      case 'Java':
+        language = javaLanguage;
+        break;
+      case 'PHP':
+        language = phpLanguage;
+        break;
+      default:
+        language = undefined;
+        break;
+    }
+    if (editor) editor.destroy();
+    if (language === undefined) {
+      editor = new EditorView({
+        parent: editorParent,
+        doc: content,
+        extensions: [basicSetup, theme, gruvboxDark],
+      });
+      return;
+    }
     editor = new EditorView({
-      doc: '',
       parent: editorParent,
-      extensions: [basicSetup, theme, gruvboxDark],
+      doc: content,
+      extensions: [basicSetup, theme, gruvboxDark, language],
     });
   });
 
@@ -69,7 +155,7 @@ export default function EditorWrapper() {
             />
           </label>
         </div>
-        <EditorOptions handleUpload={handleUpload} />
+        <EditorOptions handleUpload={handleUpload} updateLang={updateLang} />
       </div>
 
       <div
